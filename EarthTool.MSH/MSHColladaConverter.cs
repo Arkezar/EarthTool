9a225ec7ca55bc0efc93333947ac3a99dfa62f1b
Michał Gawron <arkezar@gmail.com>
2021-06-23 21:12:57 +0200

    Organize collada related code

--- EarthTool.MSH/MSHColladaConverter.cs
+++ EarthTool.MSH/MSHColladaConverter.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace EarthTool.MSH
 {
@@ -22,7 +24,7 @@ namespace EarthTool.MSH
 
     private void WriteColladaModel(Model model, string outputPath)
     {
-      var modelName = Path.GetFileNameWithoutExtension(model.FilePath);
+      var modelName = GetModelName(model);
       var resultDir = Path.Combine(outputPath, modelName);
 
       if (!Directory.Exists(resultDir))
@@ -30,145 +32,179 @@ namespace EarthTool.MSH
         Directory.CreateDirectory(resultDir);
       }
 
-      var geometriesList = model.Parts.Select((part, i) =>
+      var geometries = GetGeometries(model.Parts, modelName);
+      var scenes = GetScenes(geometries, modelName);
+      var scene = GetScene(scenes);   
+
+      var collada = new COLLADA
       {
-        var geometry = new geometry
+        Asset = new Asset
         {
-          name = $"{modelName}-Part-{i}",
-          id = $"{modelName}-Part-{i}"
-        };
-
-        var positions = GetSource("positions", part.Vertices, v => new double[] { v.Position.X, v.Position.Y, v.Position.Z });
-        var normals = GetSource("normals", part.Vertices, v => new double[] { v.Normal.X, v.Normal.Y, v.Normal.Z });
-
-        var vertices = new vertices()
-        {
-          id = "vertices",
-          input = new InputLocal[] { new InputLocal()
-            {
-              semantic = "POSITION",
-              source = "#positions"
-            }
-          }
-        };
-
-        var poly = new polylist
-        {
-          count = (ulong)part.Faces.Count,
-          input = new InputLocalOffset[] { new InputLocalOffset()
-            {
-              semantic = "VERTEX",
-              source = "#vertices",
-              offset = 0
-            },
-            new InputLocalOffset()
-            {
-              semantic = "NORMAL",
-              source = "#normals",
-              offset = 1
-            }
-          },
-          vcount = string.Join(' ', Enumerable.Repeat(3, part.Faces.Count)),
-          p = string.Join(' ', part.Faces.SelectMany(f => new string[] { f.V1.ToString(), f.V1.ToString(), f.V2.ToString(), f.V2.ToString(), f.V3.ToString(), f.V3.ToString() }))
-        };
-
-        var mesh = new mesh
-        {
-          source = new source[] { positions, normals },
-          Items = new object[] { poly },
-          vertices = vertices
-        };
-        geometry.Item = mesh;
-
-        return geometry;
-      });
-
-      var geometries = new library_geometries();
-      geometries.geometry = geometriesList.ToArray();
-
-      var visualScenes = new library_visual_scenes();
-      var visualScene = new visual_scene()
-      {
-        id = "scene"
-      };
-
-      var node = new node()
-      {
-        id = modelName,
-        name = modelName
-      };
-
-      node.instance_geometry = geometriesList.Select(g => new instance_geometry()
-      {
-        url = $"#{g.id}"
-      }).ToArray();
-      visualScene.node = new node[] { node };
-
-      visualScenes.visual_scene = new visual_scene[] { visualScene };
-      var scene = new COLLADAScene();
-      scene.instance_visual_scene = new InstanceWithExtra()
-      {
-        url = "#scene"
-      };
-
-      var collada = new Collada141.COLLADA
-      {
-        asset = new asset
-        {
-          up_axis = UpAxisType.Z_UP
+          Up_Axis = UpAxisType.Z_UP
         }
       };
 
-      collada.Items = new object[] { geometries, visualScenes };
+      collada.Library_Geometries.Add(geometries);
+      collada.Library_Visual_Scenes.Add(scenes);
+      collada.Scene = scene;
 
-      collada.scene = scene;
-      collada.Save(Path.Combine(resultDir, $"{modelName}.dae"));
+      var serializer = new XmlSerializer(typeof(COLLADA));
+      using (var stream = new FileStream(Path.Combine(resultDir, $"{modelName}.dae"), FileMode.Create))
+      {
+
+        serializer.Serialize(stream, collada);
+      }
 
       File.WriteAllText(Path.Combine(resultDir, $"{modelName}.template"), model.Template.ToString());
     }
 
-    private source GetSource<T>(string name, IList<T> data, Func<T, IEnumerable<double>> func)
+    private COLLADAScene GetScene(Library_Visual_Scenes scenes)
     {
-      var source = new source
+      var scene = new COLLADAScene();
+      scene.Instance_Visual_Scene = new InstanceWithExtra()
       {
-        id = name,
-        name = name
-      };
-      var sourceArray = new float_array
-      {
-        id = $"{name}-array",
-        count = (ulong)data.Count * 3,
-        Values = data.SelectMany(func).ToArray()
-      };
-      source.Item = sourceArray;
-      source.technique_common = new sourceTechnique_common
-      {
-        accessor = new accessor
-        {
-          source = $"#{name}-array",
-          count = (ulong)data.Count,
-          stride = 3,
-          param = new param[]
-          {
-              new param()
-              {
-                name = "X",
-                type = "float"
-              },
-              new param()
-              {
-                name = "Y",
-                type = "float"
-              },
-              new param()
-              {
-                name = "Z",
-                type = "float"
-              }
-          }
-        }
+        Url = $"#{scenes.Visual_Scene.First().Id}"
       };
 
+      return scene;
+    }
+
+    private Library_Visual_Scenes GetScenes(Library_Geometries geometries, string modelName)
+    {
+      var visualScenes = new Library_Visual_Scenes();
+      var visualScene = new Visual_Scene()
+      {
+        Id = "scene"
+      };
+
+      var node = new Node()
+      {
+        Id = modelName,
+        Name = modelName
+      };
+
+      geometries.Geometry.Select(g => new Instance_Geometry()
+      {
+        Url = $"#{g.Id}"
+      }).ToList().ForEach(g => node.Instance_Geometry.Add(g));
+
+      visualScene.Node.Add(node);
+
+      visualScenes.Visual_Scene.Add(visualScene);
+
+      return visualScenes;
+    }
+
+    private string GetModelName(Model model)
+    {
+      return Path.GetFileNameWithoutExtension(model.FilePath);
+    }
+
+    private Library_Geometries GetGeometries(IEnumerable<ModelPart> parts, string modelName)
+    {
+      var geometries = new Library_Geometries();
+
+      var geometriesList = parts.Select((part, i) => GetGeometry(part, i, modelName));
+      geometriesList.ToList().ForEach(g => geometries.Geometry.Add(g));
+
+      return geometries;
+    }
+
+    private Geometry GetGeometry(ModelPart part, int i, string modelName)
+    {
+      var geometry = new Geometry
+      {
+        Name = $"{modelName}-Part-{i}",
+        Id = $"{modelName}-Part-{i}"
+      };
+
+      var positions = GetSource("positions", part.Vertices, v => new float[] { v.Position.X, v.Position.Y, v.Position.Z });
+      var normals = GetSource("normals", part.Vertices, v => new float[] { v.Normal.X, v.Normal.Y, v.Normal.Z });
+
+      var vertices = new Vertices()
+      {
+        Id = "vertices"
+      };
+
+      vertices.Input.Add(new InputLocal()
+      {
+        Semantic = "POSITION",
+        Source = "#positions"
+      });
+
+      var poly = new Polylist
+      {
+        Count = (ulong)part.Faces.Count,
+        Vcount = string.Join(' ', Enumerable.Repeat(3, part.Faces.Count)),
+        P = string.Join(' ', part.Faces.SelectMany(f => new string[] { f.V1.ToString(), f.V1.ToString(), f.V2.ToString(), f.V2.ToString(), f.V3.ToString(), f.V3.ToString() }))
+      };
+
+      poly.Input.Add(new InputLocalOffset()
+      {
+        Semantic = "VERTEX",
+        Source = "#vertices",
+        Offset = 0
+      });
+
+      poly.Input.Add(new InputLocalOffset()
+      {
+        Semantic = "NORMAL",
+        Source = "#normals",
+        Offset = 1
+      });
+
+      var mesh = new Mesh
+      {
+        Vertices = vertices
+      };
+
+      mesh.Source.Add(positions);
+      mesh.Source.Add(normals);
+      mesh.Polylist.Add(poly);
+
+      geometry.Mesh = mesh;
+
+      return geometry;
+    }
+
+    private Source GetSource<T>(string name, IList<T> data, Func<T, IEnumerable<float>> func)
+    {
+      var source = new Source
+      {
+        Id = name,
+        Name = name
+      };
+      var sourceArray = new Float_Array
+      {
+        Id = $"{name}-array",
+        Count = (ulong)data.Count * 3,
+        Value = string.Join(" ", data.SelectMany(func).Select(v => v.ToString(System.Globalization.CultureInfo.InvariantCulture)))
+      };
+      source.Float_Array = sourceArray;
+      source.Technique_Common = new SourceTechnique_Common
+      {
+        Accessor = new Accessor
+        {
+          Source = $"#{name}-array",
+          Count = (ulong)data.Count,
+          Stride = 3
+        }
+      };
+      source.Technique_Common.Accessor.Param.Add(new Param()
+      {
+        Name = "X",
+        Type = "float"
+      });
+      source.Technique_Common.Accessor.Param.Add(new Param()
+      {
+        Name = "Y",
+        Type = "float"
+      });
+      source.Technique_Common.Accessor.Param.Add(new Param()
+      {
+        Name = "Z",
+        Type = "float"
+      });
       return source;
     }
   }
