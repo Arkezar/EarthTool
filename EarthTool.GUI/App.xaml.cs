61ac4ebc566563e83d8f082ffc79fd78d71fd66d
Michał Gawron <arkezar@gmail.com>
2021-06-30 14:41:44 +0200

    Add simple GUI

---
+++ EarthTool.GUI/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EarthTool.MSH;
+using EarthTool.MSH.Converters.Collada;
+using EarthTool.MSH.Converters.Wavefront;
+using EarthTool.TEX;
+using EarthTool.WD;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using System;
+using System.Windows;
+
+namespace EarthTool.GUI
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private readonly IHost _host;
+
+    public App()
+    {
+      _host = Initialize().Build();
+    }
+
+    private static IHostBuilder Initialize()
+    {
+      var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+      return Host.CreateDefaultBuilder()
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureLogging(config =>
+        {
+          config.AddConfiguration(configuration.GetSection("Logging"));
+          config.AddDebug();
+        })
+        .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+        {
+          containerBuilder.RegisterModule<WDModule>();
+          containerBuilder.RegisterModule<TEXModule>();
+          containerBuilder.RegisterModule<MSHModule>();
+          containerBuilder.RegisterModule<MSHColladaModule>();
+          containerBuilder.RegisterModule<MSHWavefrontModule>();
+        })
+        .ConfigureServices((ctx, services) =>
+        {
+          services.AddSingleton<MainWindow>();
+        });
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      var window = _host.Services.GetRequiredService<MainWindow>();
+      window.Show();
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+      using (_host)
+      {
+        await _host.StopAsync(TimeSpan.FromSeconds(5));
+      }
+
+      base.OnExit(e);
+    }
+  }
+}
---
+++ EarthTool.GUI/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Autofac.Features.Indexed;
+using EarthTool.Common.Interfaces;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EarthTool.GUI
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private readonly IWDExtractor _wdExtractor;
+    private readonly ITEXConverter _texConverter;
+    private readonly IIndex<string, IMSHConverter> _mshConverters;
+
+    public MainWindow(IWDExtractor wdExtractor, ITEXConverter texConverter, IIndex<string, IMSHConverter> mshConverters)
+    {
+      InitializeComponent();
+      _wdExtractor = wdExtractor;
+      _texConverter = texConverter;
+      _mshConverters = mshConverters;
+    }
+
+    private void Execute_Click(object sender, RoutedEventArgs e)
+    {
+      var inputFile = InputFileSelector.Content.ToString();
+      var outputFolder = OutputDirectorySelector.Content.ToString();
+      var fileType = System.IO.Path.GetExtension(inputFile);
+      switch (fileType)
+      {
+        case ".wd":
+          _wdExtractor.Extract(inputFile, outputFolder);
+          break;
+        case ".tex":
+          _texConverter.Convert(inputFile, outputFolder);
+          break;
+        case ".msh":
+          _mshConverters["dae"].Convert(inputFile, outputFolder);
+          break;
+      }
+    }
+
+    private void InputFileSelector_Click(object sender, RoutedEventArgs e)
+    {
+      var fileDialog = new OpenFileDialog();
+      fileDialog.Multiselect = false;
+      fileDialog.Filter = "Earth data files (*.msh, *.tex, *.wd)|*.msh;*.tex;*.wd";
+      if (fileDialog.ShowDialog() ?? false)
+      {
+        InputFileSelector.Content = fileDialog.FileName;
+      }
+    }
+
+    private void OutputDirectorySelecter_Click(object sender, RoutedEventArgs e)
+    {
+      var fileDialog = new SaveFileDialog();
+      fileDialog.FileName = System.IO.Path.GetFileName(InputFileSelector.Content.ToString());
+      fileDialog.DefaultExt = "*.*";
+
+      if (fileDialog.ShowDialog() ?? false)
+      {
+        OutputDirectorySelector.Content = System.IO.Path.GetDirectoryName(fileDialog.FileName);
+      }
+    }
+  }
+}
