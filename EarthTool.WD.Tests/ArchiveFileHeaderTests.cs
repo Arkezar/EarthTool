b2b9d8fcd931aa574660ef5d59363bba1f8ed418
Michał Gawron <arkezar@gmail.com>
2022-01-24 19:31:12 +0100

    Add initial WD tests

---
+++ EarthTool.WD.Tests/ArchiveFileHeaderTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using EarthTool.Common.Enums;
+using EarthTool.WD.Resources;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EarthTool.WD.Tests
+{
+  public class ArchiveFileHeaderTests : ArchiveTestsBase
+  {
+    ArchiveFileHeader _testHeader;
+
+    public ArchiveFileHeaderTests()
+    {
+      Fixture.Customize<ArchiveFileHeader>(c => c.FromFactory(new MethodInvoker(new GreedyConstructorQuery())));
+
+      _testHeader = Fixture.Create<ArchiveFileHeader>();
+    }
+
+    [Fact]
+    public void SerializedAndDeserializedFileHeaderShouldBeEquivalent()
+    {
+      // Given
+      var serialized = _testHeader.ToByteArray();
+
+      // When
+      using var input = new MemoryStream(serialized);
+      var deserialized = new ArchiveFileHeader(input);
+
+      // Then
+      deserialized.FileName.Should().Be(_testHeader.FileName);
+      deserialized.Offset.Should().Be(_testHeader.Offset);
+      deserialized.Length.Should().Be(_testHeader.Length);
+      deserialized.DecompressedLength.Should().Be(_testHeader.DecompressedLength);
+      deserialized.Flags.Should().Be(_testHeader.Flags);
+      deserialized.TranslationId.Should().Be(_testHeader.TranslationId);
+      deserialized.ResourceType.Should().Be(_testHeader.ResourceType);
+      deserialized.Guid.Should().Be(_testHeader.Guid);
+    }
+  }
+}
---
+++ EarthTool.WD.Tests/ArchiveTestsBase.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using AutoFixture;
+using AutoFixture.Kernel;
+using EarthTool.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Text;
+
+namespace EarthTool.WD.Tests
+{
+  public class ArchiveTestsBase
+  {
+    protected Fixture Fixture;
+
+    protected IArchiveFactory ArchiveFactory { get; }
+
+    protected ICompressor Compressor { get; }
+
+    protected IDecompressor Decompressor { get; }
+
+    public ArchiveTestsBase()
+    {
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+      Fixture = new Fixture();
+
+      var cb = new ContainerBuilder();
+      cb.RegisterModule<WDModule>();
+      cb.RegisterInstance(new NullLoggerFactory()).AsImplementedInterfaces();
+      cb.RegisterGeneric(typeof(NullLogger<>)).As(typeof(ILogger<>)).SingleInstance();
+      var container = cb.Build();
+
+      ArchiveFactory = container.Resolve<IArchiveFactory>();
+      Compressor = container.Resolve<ICompressor>();
+      Decompressor = container.Resolve<IDecompressor>();
+    }
+  }
+}
