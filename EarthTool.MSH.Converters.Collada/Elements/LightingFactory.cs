c57f05ba28937d380452754ae5287591d21d7414
Michał Gawron <arkezar@gmail.com>
2022-04-15 23:17:49 +0200

    Simplify lighting factory

--- EarthTool.MSH.Converters.Collada/Elements/LightingFactory.cs
+++ EarthTool.MSH.Converters.Collada/Elements/LightingFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Collada141;
 using EarthTool.MSH.Interfaces;
-using EarthTool.MSH.Models;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -19,64 +18,48 @@ namespace EarthTool.MSH.Converters.Collada.Elements
 
     private Node GetLightNode(ILight light, int i)
     {
-      var id = $"Light-{i}";
+      var id = GetLightName(light, i);
       var node = new Node()
       {
         Id = id,
         Name = id
       };
 
-      var rotationZdeg = light switch
+      var rotationZrad = light switch
       {
-        Models.Elements.SpotLight sl => Math.PI / 180.0 * (sl.Direction * 360.0 / 255.0),
+        Models.Elements.SpotLight sl => sl.Direction / 255f * Math.PI / 2,
         _ => 0
       };
 
-      var rotationYdeg = light switch
+      var rotationYrad = light switch
       {
-        Models.Elements.SpotLight sl => Math.PI / 180.0 * (-90 - 180 / Math.PI * sl.Tilt),
+        Models.Elements.SpotLight sl => -Math.PI / 2f - sl.Tilt,
         _ => 0
       };
 
-      var rotationZcos = (float)Math.Cos(rotationZdeg);
-      var rotationZsin = (float)Math.Sin(rotationZdeg);
-
-      var rotationZ = new Matrix4x4(rotationZcos, rotationZsin, 0, 0,
-                                    -rotationZsin, rotationZcos, 0, 0,
-                                    0, 0, 1, 0,
-                                    0, 0, 0, 1);
-
-      var rotationYcos = (float)Math.Cos(rotationYdeg);
-      var rotationYsin = (float)Math.Sin(rotationYdeg);
-
-      var rotationY = new Matrix4x4(rotationYcos, 0, -rotationYsin, 0,
-                                    0, 1, 0, 0,
-                                    rotationYsin, 0, rotationYcos, 0,
-                                    0, 0, 0, 1);
-
-      var rotation = rotationZ * rotationY;
-
-      var transformMatrix = rotation;
-      transformMatrix.Translation = light.Value;
+      var translation = Matrix4x4.CreateTranslation(light.Value);
+      var rotZ = Matrix4x4.CreateRotationZ((float)rotationZrad);
+      var rotY = Matrix4x4.CreateRotationY((float)rotationYrad);
+      var transformMatrix = rotZ * rotY * translation;
 
       node.Matrix.Add(new Matrix()
       {
         Value = string.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15}", transformMatrix.M11,
-                                                                                                                                     transformMatrix.M21,
-                                                                                                                                     transformMatrix.M31,
-                                                                                                                                     transformMatrix.M41,
-                                                                                                                                     transformMatrix.M12,
-                                                                                                                                     transformMatrix.M22,
-                                                                                                                                     transformMatrix.M32,
-                                                                                                                                     transformMatrix.M42,
-                                                                                                                                     transformMatrix.M13,
-                                                                                                                                     transformMatrix.M23,
-                                                                                                                                     transformMatrix.M33,
-                                                                                                                                     transformMatrix.M43,
-                                                                                                                                     transformMatrix.M14,
-                                                                                                                                     transformMatrix.M24,
-                                                                                                                                     transformMatrix.M34,
-                                                                                                                                     transformMatrix.M44)
+                                                                                                                                   transformMatrix.M21,
+                                                                                                                                   transformMatrix.M31,
+                                                                                                                                   transformMatrix.M41,
+                                                                                                                                   transformMatrix.M12,
+                                                                                                                                   transformMatrix.M22,
+                                                                                                                                   transformMatrix.M32,
+                                                                                                                                   transformMatrix.M42,
+                                                                                                                                   transformMatrix.M13,
+                                                                                                                                   transformMatrix.M23,
+                                                                                                                                   transformMatrix.M33,
+                                                                                                                                   transformMatrix.M43,
+                                                                                                                                   transformMatrix.M14,
+                                                                                                                                   transformMatrix.M24,
+                                                                                                                                   transformMatrix.M34,
+                                                                                                                                   transformMatrix.M44)
       });
 
       var instanceGeometry = new Instance_Light()
@@ -93,8 +76,8 @@ namespace EarthTool.MSH.Converters.Collada.Elements
     {
       return new Light()
       {
-        Id = $"Light-{i}",
-        Name = $"Light-{i}",
+        Id = GetLightName(light, i),
+        Name = GetLightName(light, i),
         Technique_Common = light switch
         {
           Models.Elements.SpotLight sl => GetSpotLight(sl),
@@ -103,6 +86,9 @@ namespace EarthTool.MSH.Converters.Collada.Elements
         }
       };
     }
+
+    private string GetLightName(ILight light, int i)
+      => $"{light.GetType().Name}-{i}";
 
     private LightTechnique_Common GetSpotLight(Models.Elements.SpotLight light)
     {
