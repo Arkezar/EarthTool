bae0af3fb7c73c67882f4713175d720a41475d05
Michał Gawron <arkezar@gmail.com>
2022-05-01 20:12:48 +0200

    Create mesh type/subtype enums.

---
+++ EarthTool.MSH/Interfaces/MeshSubType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthTool.MSH.Interfaces
+{
+  public enum MeshSubType
+  {
+    Unknown = 0,
+    Unit = 1,
+    Building = 2
+  }
+}
---
+++ EarthTool.MSH/Interfaces/MeshType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthTool.MSH.Interfaces
+{
+  public enum MeshType
+  {
+    Regular = 0,
+    Dynamic = 1
+  }
+}
