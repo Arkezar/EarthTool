59351901f0b0350ed4e934485477b20a4fd1a9fe
Michał Gawron <arkezar@gmail.com>
2022-10-05 20:20:56 +0200

    Remove obj from MSH command

--- EarthTool.MSH/MSHCommand.cs
+++ EarthTool.MSH/MSHCommand.cs
@@ -11,24 +11,22 @@ namespace EarthTool.Commands
 {
   public class MSHCommand : Command
   {
-    private readonly IIndex<string, IMSHConverter> _converter;
+    private readonly IMSHConverter _converter;
     private readonly ILogger<MSHCommand> _logger;
 
-    public MSHCommand(IIndex<string, IMSHConverter> converter, ILogger<MSHCommand> logger) : base("msh", "Convert MSH files to 3D gfx file formats")
+    public MSHCommand(IMSHConverter converter, ILogger<MSHCommand> logger) : base("msh", "Convert MSH files to 3D gfx file formats")
     {
       _converter = converter;
       _logger = logger;
 
       var input = new Argument<string>("input", "MSH file path");
       var output = new Option<string>(new[] { "--output", "-o" }, "Output directory. Current if not specified.");
-      var format = new Option<string>(new[] { "--format", "-f" }, () => "dae", "Output format. obj for Wavefront, dae for Collada");
       AddArgument(input);
       AddOption(output);
-      AddOption(format);
-      Handler = CommandHandler.Create<string, string, string>(HandleCommand);
+      Handler = CommandHandler.Create<string, string>(HandleCommand);
     }
 
-    private void HandleCommand(string input, string output, string format)
+    private void HandleCommand(string input, string output)
     {
       var path = Path.GetDirectoryName(input);
       if (string.IsNullOrEmpty(path))
@@ -42,7 +40,7 @@ namespace EarthTool.Commands
       {
         try
         {
-          _converter[format].Convert(filePath, output);
+          _converter.Convert(filePath, output);
           _logger.LogInformation("Processed file {FilePath}", filePath);
         }
         catch (Exception e)
