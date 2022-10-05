338c41b284c2e7514f42951463abc5eb98a4ec6d
Michał Gawron <arkezar@gmail.com>
2022-10-05 20:06:31 +0200

    Remove GUI projects

--- EarthTool.GUI.WPF/Converters/WpfFileFlagsValueConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using EarthTool.GUI.Core.Converters;
-using MvvmCross.Platforms.Wpf.Converters;
-
-namespace EarthTool.GUI.WPF.Converters
-{
-  public class WpfFileFlagsValueConverter : MvxNativeValueConverter<FileFlagsValueConverter>
-  {
-  }
-}
